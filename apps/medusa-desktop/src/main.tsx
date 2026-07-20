3d185b7f83e01cea6fb8db60c32250c9c37b91b6
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-07-20 20:19:40 +0200

    Add recent sessions browser to desktop (#90)
    * Add desktop recent-session browser
    
    * Style desktop session browser
    
    * Mount desktop session browser

--- apps/medusa-desktop/src/main.tsx
+++ apps/medusa-desktop/src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { SessionDock } from "./SessionDock";
 import "./styles.css";
 import "./medusa-desktop.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <SessionDock />
   </React.StrictMode>,
 );
