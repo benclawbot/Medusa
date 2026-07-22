360e343171521ad7bf180e4f6ec994dd1c40dab0
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-07-22 11:31:37 +0200

    Mount desktop memory dock

--- apps/medusa-desktop/src/main.tsx
+++ apps/medusa-desktop/src/main.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { DiffDock } from "./DiffDock";
+import { MemoryDock } from "./MemoryDock";
 import { SessionDock } from "./SessionDock";
 import "./styles.css";
 import "./medusa-desktop.css";
 import "./diff-dock.css";
+import "./memory-browser.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
     <SessionDock />
     <DiffDock />
+    <MemoryDock />
   </React.StrictMode>,
 );
