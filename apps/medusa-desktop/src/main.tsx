b01515ba12dde7207927c268abcbb785f05e695a
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-07-22 10:05:34 +0200

    Mount desktop diff dock

--- apps/medusa-desktop/src/main.tsx
+++ apps/medusa-desktop/src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { DiffDock } from "./DiffDock";
 import { SessionDock } from "./SessionDock";
 import "./styles.css";
 import "./medusa-desktop.css";
+import "./diff-dock.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
     <SessionDock />
+    <DiffDock />
   </React.StrictMode>,
 );
