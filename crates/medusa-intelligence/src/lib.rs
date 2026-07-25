c6a3f4feed89eab4bc95cfa6f8d3aa5da2bb88be
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-07-25 19:05:58 +0200

    feat(intelligence): export LSP lifecycle APIs

--- crates/medusa-intelligence/src/lib.rs
+++ crates/medusa-intelligence/src/lib.rs
@@ -7,6 +7,7 @@ mod graph;
 mod impact;
 mod index;
 mod language;
+mod lsp;
 mod module_graph;
 mod patch;
 mod rename;
@@ -25,6 +26,9 @@ pub use graph::{CallEdge, DependencyEdge, SemanticGraph, SymbolId};
 pub use impact::{TestImpact, select_tests, select_tests_with_index};
 pub use index::IndexRefresh;
 pub use language::{CodeIndex, Language, Reference, Symbol, SymbolKind};
+pub use lsp::{
+    LspClient, LspError, LspServerConfig, LspServerManager, LspServerState,
+};
 pub use module_graph::{RustDependencyEdge, RustDependencyKind, RustModuleGraph};
 pub use patch::{
     PatchTransaction, TextEdit, TransactionReceipt, finalize_patch_transactions,
