ebec32b0d152d0474d437a78f08725348822d88a
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-07-25 08:15:46 +0200

    feat(desktop): mount unified execution timeline

--- apps/medusa-desktop/src/main.tsx
+++ apps/medusa-desktop/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { DesktopTimelineBridge } from "./DesktopTimelineBridge";
 import { DiffDock } from "./DiffDock";
 import { MemoryDock } from "./MemoryDock";
 import { SessionDock } from "./SessionDock";
@@ -14,6 +15,7 @@ import "./desktop-ux-overhaul.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <DesktopTimelineBridge />
     <SessionDock />
     <DiffDock />
     <MemoryDock />
