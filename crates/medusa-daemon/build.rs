ed8153b66719f2027db008f3ed7c0083c76484cc
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-08-01 03:17:28 +0200

    Apply final Telegram compile fixups

--- crates/medusa-daemon/build.rs
+++ crates/medusa-daemon/build.rs
@@ -1,10 +1,14 @@
 mod build_fixups;
 #[rustfmt::skip]
+mod build_compile_fixups;
+#[rustfmt::skip]
 mod build_mini_app_client;
 #[rustfmt::skip]
 mod build_mini_app_integration;
 #[rustfmt::skip]
 mod build_native;
+#[rustfmt::skip]
+mod build_security;
 mod build_support;
 #[rustfmt::skip]
 mod build_voice_integration;
@@ -14,9 +18,11 @@ mod build_webhook_integration;
 fn main() {
     build_fixups::run();
     build_mini_app_client::run();
+    build_security::run();
     build_support::run();
     build_native::run();
     build_voice_integration::run();
     build_mini_app_integration::run();
     build_webhook_integration::run();
+    build_compile_fixups::run();
 }
