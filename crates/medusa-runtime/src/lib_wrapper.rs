1924dea53d9b801121dd96b8b477bd22f4c80831
BenClawBot (AlphaOM3G1) <benclawbot@gmail.com>
2026-08-10 12:59:03 +0200

    feat: register parallel mutation batch runtime

--- crates/medusa-runtime/src/lib_wrapper.rs
+++ crates/medusa-runtime/src/lib_wrapper.rs
@@ -3,6 +3,7 @@ include!("lib.rs");
 #[rustfmt::skip]
 mod parent_reviewer;
 mod parallel_mutation;
+mod parallel_mutation_batch;
 
 pub mod openai_realtime_session;
 pub mod openai_realtime_websocket;
